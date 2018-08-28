11c5cc96a4cb16e142cdf0e7e4dff76768fd7f30
claudio serrano <claudio.serrano@globant.com>
2018-08-28 00:47:36 -0300

    agregando docker file para mysql

---
+++ mysql_img/script.sql
@@ -0,0 +1,6 @@
+create table users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, phone_number TEXT);
+
+INSERT INTO users (name, phone_number) values ('claudio', '+132133123');
+INSERT INTO users (name, phone_number) values ('claudio1', '+132133123');
+INSERT INTO users (name, phone_number) values ('claudio2', '+132133123');
+INSERT INTO users (name, phone_number) values ('claudio3', '+132133123');
